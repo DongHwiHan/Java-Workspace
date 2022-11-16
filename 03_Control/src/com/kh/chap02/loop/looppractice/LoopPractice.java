2ae90ccea5bb1a3fa081059919aca044f41c22df
HanDofasdsa <117423329+HanDofasdsa@users.noreply.github.com>
2022-11-16 15:19:00 +0900

    배열 / 이차배열

--- 03_Control/src/com/kh/chap02/loop/looppractice/LoopPractice.java
+++ 03_Control/src/com/kh/chap02/loop/looppractice/LoopPractice.java
@@ -6,9 +6,9 @@ public class LoopPractice {
 
 	public void practice1() {
 
-//		사용자로부터 한 개의 값을 입력 받아 1부터 그 숫자까지의 숫자들을 모두 출력하세요.
-//		단, 입력한 수는 1보다 크거나 같아야 합니다. 만일, 1 미만의 숫자가 입력됐다면
-//		“1 이상의 숫자를 입력해주세요”가 출력되면서 다시 사용자가 값을 입력하도록 하세요.
+		//		사용자로부터 한 개의 값을 입력 받아 1부터 그 숫자까지의 숫자들을 모두 출력하세요.
+		//		단, 입력한 수는 1보다 크거나 같아야 합니다. 만일, 1 미만의 숫자가 입력됐다면
+		//		“1 이상의 숫자를 입력해주세요”가 출력되면서 다시 사용자가 값을 입력하도록 하세요.
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("1 이상의 숫자를 입력하세요 : ");
@@ -30,9 +30,9 @@ public class LoopPractice {
 
 	public void practice2() {
 
-//		사용자로부터 한 개의 값을 입력 받아 1부터 그 숫자까지의 모든 숫자를 거꾸로 출력하세요.
-//		단, 입력한 수는 1보다 크거나 같아야 합니다. 만일 , 1 미만의 숫자가 입력됐다면
-//		“1 이상의 숫자를 입력해주세요”가 출력되면서 다시 사용자가 값을 입력하도록 하세요.
+		//		사용자로부터 한 개의 값을 입력 받아 1부터 그 숫자까지의 모든 숫자를 거꾸로 출력하세요.
+		//		단, 입력한 수는 1보다 크거나 같아야 합니다. 만일 , 1 미만의 숫자가 입력됐다면
+		//		“1 이상의 숫자를 입력해주세요”가 출력되면서 다시 사용자가 값을 입력하도록 하세요.
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("1 이상의 숫자를 입력하세요 : ");
@@ -54,10 +54,10 @@ public class LoopPractice {
 
 	public void practice3() {
 
-//		1부터 사용자에게 입력 받은 수까지의 정수들의 합을 출력하세요.
-//		ex.
-//		정수를 하나 입력하세요 : 8
-//		1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 = 36
+		//		1부터 사용자에게 입력 받은 수까지의 정수들의 합을 출력하세요.
+		//		ex.
+		//		정수를 하나 입력하세요 : 8
+		//		1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 = 36
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("정수를 하나 입력하세요 : ");
@@ -77,11 +77,11 @@ public class LoopPractice {
 
 	public void practice4() {
 
-//		사용자로부터 두 개의 값을 입력 받아 그 사이의 숫자를 모두 출력하세요. 만일, 1 미만의 숫자가 입
-//		력됐다면 “1 이상의 숫자를 입력해주세요”가 출력되면서 다시 사용자가 값을 입력하도록 하세요.
-//		 첫 번째 숫자 : 8  첫 번째 숫자 : 4  첫 번째 숫자 : 9
-//		 두 번째 숫자 : 4  두 번째 숫자 : 8  두 번째 숫자 : 0
-//		 4 5 6 7 8      4 5 6 7 8     1이상의 숫자를 입력해주세요.
+		//		사용자로부터 두 개의 값을 입력 받아 그 사이의 숫자를 모두 출력하세요. 만일, 1 미만의 숫자가 입
+		//		력됐다면 “1 이상의 숫자를 입력해주세요”가 출력되면서 다시 사용자가 값을 입력하도록 하세요.
+		//		 첫 번째 숫자 : 8  첫 번째 숫자 : 4  첫 번째 숫자 : 9
+		//		 두 번째 숫자 : 4  두 번째 숫자 : 8  두 번째 숫자 : 0
+		//		 4 5 6 7 8      4 5 6 7 8     1이상의 숫자를 입력해주세요.
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("첫 번째 숫자 : ");
@@ -108,8 +108,8 @@ public class LoopPractice {
 
 	public void practice5() {
 
-//		사용자로부터 입력 받은 숫자의 단부터 9단까지 출력하세요.단 , 9를 초과하는 숫자가 입력됐다면
-//		“9 이하의 숫자를 입력해주세요”가 출력되면서 다시 사용자가 값을 입력하도록 하세요.
+		//		사용자로부터 입력 받은 숫자의 단부터 9단까지 출력하세요.단 , 9를 초과하는 숫자가 입력됐다면
+		//		“9 이하의 숫자를 입력해주세요”가 출력되면서 다시 사용자가 값을 입력하도록 하세요.
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("숫자 : ");
@@ -131,12 +131,12 @@ public class LoopPractice {
 
 	public void practice6() {
 
-//		사용자로부터 시작 숫자와 공차를 입력 받아
-//		일정한 값으로 숫자가 커지거나 작아지는 프로그램을 구현하세요.
-//		단, 출력되는 숫자는 총 10개입니다.
-//		시작 숫자 : 4
-//		공차 : 3
-//		4 7 10 13 16 19 22 25 28 31
+		//		사용자로부터 시작 숫자와 공차를 입력 받아
+		//		일정한 값으로 숫자가 커지거나 작아지는 프로그램을 구현하세요.
+		//		단, 출력되는 숫자는 총 10개입니다.
+		//		시작 숫자 : 4
+		//		공차 : 3
+		//		4 7 10 13 16 19 22 25 28 31
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("시작 숫자 : ");
@@ -155,11 +155,11 @@ public class LoopPractice {
 
 	public void practice7() {
 
-//		정수 입력 : 4
-//		*
-//		**
-//		***
-//		****
+		//		정수 입력 : 4
+		//		*
+		//		**
+		//		***
+		//		****
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("정수 입력 : ");
@@ -176,11 +176,11 @@ public class LoopPractice {
 
 	public void practice8() {
 
-//		정수 입력 : 4
-//		****
-//		***
-//		**
-//		*
+		//		정수 입력 : 4
+		//		****
+		//		***
+		//		**
+		//		*
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("정수 입력 : ");
@@ -196,11 +196,11 @@ public class LoopPractice {
 
 	public void practice9() {
 
-//		정수 입력 : 4
-//		   *
-//		  **
-//		 ***
-//		****
+		//		정수 입력 : 4
+		//		   *
+		//		  **
+		//		 ***
+		//		****
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("정수 입력 : ");
@@ -222,11 +222,11 @@ public class LoopPractice {
 
 	public void practice10() {
 
-//		정수 입력 : 4
-//		****
-//		 ***
-//		  **
-//		   *
+		//		정수 입력 : 4
+		//		****
+		//		 ***
+		//		  **
+		//		   *
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("정수 입력 : ");
@@ -246,11 +246,11 @@ public class LoopPractice {
 
 	public void practice11() {
 
-//		정수 입력 : 4
-//		   *
-//		  ***
-//		 *****
-//		*******
+		//		정수 입력 : 4
+		//		   *
+		//		  ***
+		//		 *****
+		//		*******
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("정수 입력 : ");
@@ -269,10 +269,10 @@ public class LoopPractice {
 
 	public void practice12() {
 
-//		사용자로부터 입력 받은 하나의 값이 소수인지 판별하는 프로그램을 구현하세요.
-//		단, 입력한 수가 2보다 작은 경우 “잘못 입력하셨습니다.”를 출력하세요.
-//		숫자 : 5 숫자 : 9 숫자 : 0
-//		소수입니다. 소수가 아닙니다. 잘못 입력하셨습니다.
+		//		사용자로부터 입력 받은 하나의 값이 소수인지 판별하는 프로그램을 구현하세요.
+		//		단, 입력한 수가 2보다 작은 경우 “잘못 입력하셨습니다.”를 출력하세요.
+		//		숫자 : 5 숫자 : 9 숫자 : 0
+		//		소수입니다. 소수가 아닙니다. 잘못 입력하셨습니다.
 
 		Scanner sc = new Scanner(System.in);
 
@@ -296,11 +296,11 @@ public class LoopPractice {
 
 	public void practice13() {
 
-//		2부터 사용자가 입력한 수까지의 소수를 모두 출력하고 소수의 개수를 출력하세요.
-//		단, 입력한 수가 2보다 작은 경우 “잘못 입력하셨습니다.”를 출력하세요.
-//		숫자 : 11
-//		2 3 5 7 11
-//		2부터 11까지 소수의 개수는 5개입니다.
+		//		2부터 사용자가 입력한 수까지의 소수를 모두 출력하고 소수의 개수를 출력하세요.
+		//		단, 입력한 수가 2보다 작은 경우 “잘못 입력하셨습니다.”를 출력하세요.
+		//		숫자 : 11
+		//		2 3 5 7 11
+		//		2부터 11까지 소수의 개수는 5개입니다.
 
 		Scanner sc = new Scanner(System.in);
 
@@ -332,11 +332,11 @@ public class LoopPractice {
 
 	public void practice14() {
 
-//		2부터 사용자가 입력한 수까지의 소수를 모두 출력하고 소수의 개수를 출력하세요.
-//		단, 입력한 수가 2보다 작은 경우 “잘못 입력하셨습니다.”를 출력하세요.
-//		숫자 : 11
-//		2 3 5 7 11
-//		2부터 11까지 소수의 개수는 5개입니다.
+		//		2부터 사용자가 입력한 수까지의 소수를 모두 출력하고 소수의 개수를 출력하세요.
+		//		단, 입력한 수가 2보다 작은 경우 “잘못 입력하셨습니다.”를 출력하세요.
+		//		숫자 : 11
+		//		2 3 5 7 11
+		//		2부터 11까지 소수의 개수는 5개입니다.
 
 		Scanner sc = new Scanner(System.in);
 
@@ -359,12 +359,12 @@ public class LoopPractice {
 
 	public void practice15() {
 
-//		정수 입력 : 3
-//		*
-//		**
-//		***
-//		**
-//		*
+		//		정수 입력 : 3
+		//		*
+		//		**
+		//		***
+		//		**
+		//		*
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("정수 입력 : ");
@@ -388,61 +388,110 @@ public class LoopPractice {
 
 	public void practice16() {
 
-//		정수 입력 : 5
-//		*****
-//		* 	*
-//		* 	*
-//		* 	*
-//		*****
+		//		정수 입력 : 5
+		//		*****
+		//		* 	*
+		//		* 	*
+		//		* 	*
+		//		*****
+		Scanner sc = new Scanner(System.in);
+		System.out.print("정수 입력: ");
+		int square = sc.nextInt();
+		for (int i = 1; i <= square; i++) {
+			if (i == 1 || i == square) {
+				for (int j = 0; j < square; j++) {
+					System.out.print('*');
+				}
+				System.out.println();
 
+			} else {
+				for (int j = 1; j <= square; j++) {
+					if (j == 1 || j == square) {
+						System.out.print('*');
+					} else {
+						System.out.print(" ");
+					}
+				}
+				System.out.println();
+			}
+		}
 	}
 
 	public void practice17() {
 
-//		정수 입력 : 4
-//		*
-//	   ***
-//	  *****
-//	 *******
-//	  *****
-//	   ***
-//		*
+		//		정수 입력 : 4
+		//		*
+		//	   ***
+		//	  *****
+		//	 *******
+		//	  *****
+		//	   ***
+		//		*
 		Scanner sc = new Scanner(System.in);
 		System.out.println("정수 입력 : ");
 		int num = sc.nextInt();
-		for(int i = 0 ; i<num; i++) {
-			for (int j = 0; j < num - (1+i); j++) { // 0 1
+		for (int i = 0; i < num; i++) {
+			for (int j = 0; j < num - (1 + i); j++) { // 0 1
 				System.out.print(" "); // ;
 			}
-			for (int j = 0; j < 1+2*i; j++) { // 0 
+			for (int j = 0; j < 1 + 2 * i; j++) { // 0
 				System.out.print("*");// *
 			}
 			System.out.println();
 		}
-		for(int i =0; i<num-1; i++) { // 0 1 2 ...
-			
-			for (int j = 0; j < 1+i; j++) { // 0 
+		for (int i = 0; i < num - 1; i++) { // 0 1 2 ...
+
+			for (int j = 0; j < 1 + i; j++) { // 0
 				System.out.print(" "); // ;
 			}
-			for (int j = 0; j < (num-i)*2-3; j++) 
-			{ // 1+2*(num -2 - i) -> 1 -> 0
-//			  1 + 2num - 4 -2ㅑ -> 2(num)
+			for (int j = 0; j < (num - i) * 2 - 3; j++) { // 1+2*(num -2 - i) -> 1 -> 0
+				//			  1 + 2num - 4 -2ㅑ -> 2(num)
 				System.out.print("*");// *****
 			}
 			System.out.println();
 		}
-	}	
-		
+	}
+
 	public void practice18() {
 
-//		정수 입력 : 4
-//		*
-//	   * *
-//	  *   *
-//	 *     *
-//	  *   *
-//	   * *
-//		*
+		//		정수 입력 : 4
+		//		*
+		//	   * *
+		//	  *   *
+		//	 *     *
+		//	  *   *
+		//	   * *
+		//		*
+		Scanner sc = new Scanner(System.in);
+		System.out.print("정수 입력 : ");
+		int star = sc.nextInt();
+
+		for (int i = 0; i < star; i++) {
+			for (int j = 0; j < star - (i + 1); j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j <= 2 * i; j++) {
+				if (j == 0 || j == 2 * i) {
+					System.out.print('*');
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		for (int i = 1; i < star; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < 2 * (star - i) - 1; j++) {
+				if (j == 0 || j == 2 * (star - i) - 2) {
+					System.out.print('*');
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
 
 	}
 
--- 03_Control/src/com/kh/chap03/branch/A_Break.java
+++ 03_Control/src/com/kh/chap03/branch/A_Break.java
@@ -4,63 +4,134 @@ import java.util.Scanner;
 
 public class A_Break {
 
-//	break; : break문을 만나는 순간 현재 속해있는 반복문을 빠져나감.
-//	->즉, 가장 가까운 반복문만 빠져나감.
-	
-//	주의할점 : switch문 안의 break 와는 다른개념
-//	 	 	switch문 안의 break는 해당 switch문만 빠져나가는 용도.
-	
+	// break; : break문을 만나는 순간 현재 속해있는 반복문을 빠져나감.
+	// ->즉, 가장 가까운 반복문만 빠져나감.
+
+	// 주의할점 : switch문 안의 break 와는 다른개념
+	// switch문 안의 break는 해당 switch문만 빠져나가는 용도.
+
 	public void method1() {
-//		매번 반복적으로 발생되는 랜덤값(1~100) 출력
-//		그 랜덤값이 3의 배수일경우에만 반복문을 종료.
-		
-		while(true) {
-//			랜덤값 추출
-			int random =(int)(Math.random() * 100 +1);
-//			랜덤값을 출력
-			System.out.println("random : "+ random);
-//			랜덤값이 3의 배수인지 검사
-			if(random%3==0) {
+		// 매번 반복적으로 발생되는 랜덤값(1~100) 출력
+		// 그 랜덤값이 3의 배수일경우에만 반복문을 종료.
+
+		while (true) {
+			// 랜덤값 추출
+			int random = (int) (Math.random() * 100 + 1);
+			// 랜덤값을 출력
+			System.out.println("random : " + random);
+			// 랜덤값이 3의 배수인지 검사
+			if (random % 3 == 0) {
 				break;
 			}
 		}
 	}
-	
+
 	public void method2() {
-//		매번 사용자에게 문자열을 입력받은 후 해당 문자열의 길이 출력
-//		단, 사용자가 입력한 문자열이 "exit"인 경우 반복문을 종료
+		// 매번 사용자에게 문자열을 입력받은 후 해당 문자열의 길이 출력
+		// 단, 사용자가 입력한 문자열이 "exit"인 경우 반복문을 종료
 		Scanner sc = new Scanner(System.in);
-		
-		
-		while(true) {
+
+		while (true) {
 			System.out.println("문자열 입력 : ");
 			String str = sc.nextLine();
-			
-			if(str.equals("exit")){
+
+			if (str.equals("exit")) {
 				break;
 			}
-			System.out.println(str+ "의 길이는 : " + str.length());
+			System.out.println(str + "의 길이는 : " + str.length());
 		}
 	}
-	
+
 	public void method3() {
-//		매번 반복적으로 사용자에게 양수를 입력받아 1~ 사용자가 입력한 수까지 출력
-//		단, 정상적으로 양수를 입력했을시 1~사용자가 입력한수까지 출력하고 종료
+		// 매번 반복적으로 사용자에게 양수를 입력받아 1~ 사용자가 입력한 수까지 출력
+		// 단, 정상적으로 양수를 입력했을시 1~사용자가 입력한수까지 출력하고 종료
 		Scanner sc = new Scanner(System.in);
-		
-		while(true) {
+
+		while (true) {
 			System.out.println("양수를 입력 : ");
 			int num = sc.nextInt();
-			
-			if(num > 0) {// 양수로 제대로 입력한 경우 -> 1~num 출력후 종료
-				for(int i = 1; i <= num; i++) {
-					System.out.println(i+ " ");
+
+			if (num > 0) {// 양수로 제대로 입력한 경우 -> 1~num 출력후 종료
+				for (int i = 1; i <= num; i++) {
+					System.out.println(i + " ");
 				}
 				break;
-			}else {// 잘못입력시 -> 다시 입력하라는 출력문
+			} else {// 잘못입력시 -> 다시 입력하라는 출력문
 				System.out.println("잘못입력했습니다. 다시입력해주세여.");
 			}
 		}
 	}
 
+	public void method14() {
+
+		Scanner sc = new Scanner(System.in);
+
+		String n = "";
+		while (true) {
+			System.out.print("문자열 입력 : ");
+			String str = sc.nextLine();
+
+			if (str.equals("stop")) {
+				System.out.print("결과 : " + n);
+				break;
+			}
+			n += str + " ";
+			System.out.println("결과 : " + n);
+		}
+	}
+
+	public void method15() {
+
+		Scanner sc = new Scanner(System.in);
+
+		int q = 0;
+		int w = 0;
+		int e = 0;
+
+		while (true) {
+			System.out.println("======== 메뉴 ========");
+			System.out.println("1. 아메리카노    3500원");
+			System.out.println("2. 카페라떼     4100원");
+			System.out.println("3. 바닐라라떼    4300원");
+			System.out.println("====================");
+
+			System.out.println("메뉴 선택 : ");
+			int m = sc.nextInt();
+			System.out.println("수량 선택 : ");
+			int c = sc.nextInt();
+
+			switch (m) {
+			case 1:
+				q += c;
+				break;
+			case 2:
+				w += c;
+				break;
+			case 3:
+				e += c;
+				break;
+			default:
+				System.out.println("잘못 선택하셨습니다. 다시 선택해주세요.");
+				continue;
+			}
+			System.out.println("추가 주문 하시겠습니까? (Y/N) : ");
+			char ch = sc.next().charAt(0);
+
+			if (ch == 'N' || ch == 'n') {
+				System.out.println("=================");
+				if (q != 0) {
+					System.out.println("아메리카노" + q + "잔 : " + 3500 * q + "원");
+				}
+				if (w != 0) {
+					System.out.println("카페라떼" + w + "잔 : " + 4100 * w + "원");
+				}
+				if (e != 0) {
+					System.out.println("바닐라라떼" + e + "잔 : " + 4300 * e + "원");
+				}
+				System.out.println("=================");
+				System.out.println("총액 : " + (3500 * q + 4100 * w + 4300 * e) + "원");
+				break;
+			}
+		}
+	}
 }
