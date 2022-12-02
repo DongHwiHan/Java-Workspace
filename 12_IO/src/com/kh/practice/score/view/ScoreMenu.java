b04399b413cc4c55824fc4dbfe85b9405b3ffba6
HanDofasdsa <117423329+HanDofasdsa@users.noreply.github.com>
2022-12-02 15:16:40 +0900

    IO 객체 입,출력/컬렉션

---
+++ 12_IO/src/com/kh/practice/score/view/ScoreMenu.java
@@ -0,0 +1,43 @@
+package com.kh.practice.score.view;
+
+import java.util.Scanner;
+
+import com.kh.practice.score.controller.ScoreController;
+
+public class ScoreMenu {
+	Scanner sc = new Scanner(System.in);
+	ScoreController scr = new ScoreController();
+
+	public void mainMenu() {
+		while (true) {
+			System.out.println("1. 성적 저장");
+			System.out.println("2. 성적 출력");
+			System.out.println("9. 끝내기"); // ➔ “프로그램을 종료합니다.” 출력 후 종료
+			System.out.print("메뉴 번호 : ");
+			int num = Integer.parseInt(sc.nextLine());
+			switch (num) {
+			case 1:
+				saveScore();
+				break;
+
+			case 2:
+				readScore();
+				break;
+			case 9:
+				System.out.println("프로그램을 종료합니다.");
+				return;
+			default:
+				System.out.println("다시 입력해주세요");
+			}
+			// 잘못 입력했을 경우 “잘못 입력하셨습니다. 다시 입력해주세요.” 출력 후 반복
+		}
+	}
+
+	public void saveScore() {
+
+	}
+
+	public void readScore() {
+
+	}
+}
